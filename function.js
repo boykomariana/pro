4c12422f395080f10c40e104a19771b8ce11d2a5
boykomariana <145941663+boykomariana@users.noreply.github.com>
2023-09-24 23:38:51 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,13 @@
+document.getElementById("telefono").onclick = function () {
+    console.log("telefono");
+    document.getElementById("telefono").innerHTML = "575 501-6422";
+}
+
+document.getElementById("direccion").onclick =function () {
+    console.log("direccion");
+    document.getElementById("direccion"). innerHTML = "8275 Lovers Ln";
+}
+document.getElementById("mi_correo").onclick =function () {
+    console.log("mi_correo");
+    document.getElementById("mi_correo"). innerHTML = "leta.simpson@example.com";
+}
